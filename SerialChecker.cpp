6ced310f392f5437936045643df69d929baf4cf5
Ashutosh <161562995+Ashutosh0x@users.noreply.github.com>
2024-04-11 17:00:05 +0530

    SerialChecker.cpp

--- SerialChecker.cpp
+++ SerialChecker.cpp
@@ -9,9 +9,11 @@ using std::endl;
 const int DELAY_SHORT = 1000;
 const int DELAY_LONG = 10000;
 
+
 void displayMessage(const std::string& message) {
-    cout << message << endl;
+    cout << "\033[1;32m" << message << "\033[0m" << endl; // Green color
 }
+
 
 void checkSystemInfo(const std::string& command, const std::string& title) {
     displayMessage(title);
@@ -20,11 +22,14 @@ void checkSystemInfo(const std::string& command, const std::string& title) {
 }
 
 int main() {
+    cout << "\033[1;34m"; 
     displayMessage("Checking your serials...");
     displayMessage("Please Wait.");
+    cout << "\033[0m"; 
 
     Sleep(DELAY_SHORT);
 
+    // Check various system information
     checkSystemInfo("wmic diskdrive get serialnumber", "Disk");
     checkSystemInfo("wmic baseboard get serialnumber", "Motherboard");
     checkSystemInfo("wmic path win32_computersystemproduct get uuid", "SMBios");
@@ -34,7 +39,9 @@ int main() {
     checkSystemInfo("wmic cpu get processorid", "CPU");
     checkSystemInfo("wmic path Win32_NetworkAdapter where \"PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'\" get MacAddress", "Mac");
 
+    cout << "\033[1;34m"; 
     displayMessage("Exiting in 10 seconds.");
+    cout << "\033[0m"; 
     Sleep(DELAY_LONG);
     return 0;
 }
